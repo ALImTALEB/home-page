5d0aa83ef7cf824bcfe66a09004a0854a9a4526a
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2025-03-29 14:41:28 +0100

    fix css

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -4,26 +4,129 @@ import "./Story.css";
 
 const Story = () => {
   return (
-    <section>
-      <div className="firstContainer">
-        <h2>Member stories</h2>
-        <div className="containerr">
-          <div className="left">
-            <div className="cardContainer">
-              <figure>
-                <img className="img" src={story1} alt="" />
-              </figure>
-              <div className="details">
+    <>
+      <section className="sectionMobile">
+        <div className="firstContainer">
+          <h2>Member stories</h2>
+          <div className="containerr">
+            <div className="left">
+              <div className="cardContainer">
                 <h3 className="title">John's Story</h3>
-                <p className="paragraph">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-                  aliquam erat{" "}
-                </p>
+                <div className="details">
+                  <figure>
+                    <img className="img" src={story1} alt="" />
+                  </figure>
+                  <p className="paragraph">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                    magna aliquam erat{" "}
+                  </p>
+                </div>
+                <button className="readMore">Read more</button>
+              </div>
+              <div>
+                <div className="cardContainer">
+                  <h3 className="title">John's Story</h3>
+                  <div className="details">
+                    <figure>
+                      <img className="img" src={story1} alt="" />
+                    </figure>
+                    <p className="paragraph">
+                      Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                      sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                      magna aliquam erat{" "}
+                    </p>
+                  </div>
+                  <button className="readMore">Read more</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="right">
+              <div className="cardContainer">
+                <h3 className="title">John's Story</h3>
+                <div className="details">
+                  <figure>
+                    <img className="img" src={story1} alt="" />
+                  </figure>
+                  <p className="paragraph">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                    magna aliquam erat{" "}
+                  </p>
+                </div>
+                <button className="readMore">Read more</button>
+              </div>
+              <div className="cardContainer">
+                <h3 className="title">John's Story</h3>
+                <div className="details">
+                  <figure>
+                    <img className="img" src={story1} alt="" />
+                  </figure>
+                  <p className="paragraph">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                    magna aliquam erat{" "}
+                  </p>
+                </div>
                 <button className="readMore">Read more</button>
               </div>
             </div>
-            <div>
+          </div>
+        </div>
+      </section>
+      <section className="sectionWeb">
+        <div className="firstContainer">
+          <h2>Member stories</h2>
+          <div className="containerr">
+            <div className="left">
+              <div className="cardContainer">
+                <figure>
+                  <img className="img" src={story1} alt="" />
+                </figure>
+                <div className="details">
+                  <h3 className="title">John's Story</h3>
+                  <p className="paragraph">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                    magna aliquam erat{" "}
+                  </p>
+                  <button className="readMore">Read more</button>
+                </div>
+              </div>
+              <div>
+                <div className="cardContainer">
+                  <figure>
+                    <img className="img" src={story1} alt="" />
+                  </figure>
+                  <div className="details">
+                    <h3 className="title">John's Story</h3>
+                    <p className="paragraph">
+                      Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                      sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                      magna aliquam erat{" "}
+                    </p>
+                    <button className="readMore">Read more</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="right">
+              <div className="cardContainer">
+                <figure>
+                  <img className="img" src={story1} alt="" />
+                </figure>
+                <div className="details">
+                  <h3 className="title">John's Story</h3>
+                  <p className="paragraph">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                    magna aliquam erat{" "}
+                  </p>
+                  <button className="readMore">Read more</button>
+                </div>
+              </div>
               <div className="cardContainer">
                 <figure>
                   <img className="img" src={story1} alt="" />
@@ -40,40 +143,9 @@ const Story = () => {
               </div>
             </div>
           </div>
-
-          <div className="right">
-            <div className="cardContainer">
-              <figure>
-                <img className="img" src={story1} alt="" />
-              </figure>
-              <div className="details">
-                <h3 className="title">John's Story</h3>
-                <p className="paragraph">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-                  aliquam erat{" "}
-                </p>
-                <button className="readMore">Read more</button>
-              </div>
-            </div>
-            <div className="cardContainer">
-              <figure>
-                <img className="img" src={story1} alt="" />
-              </figure>
-              <div className="details">
-                <h3 className="title">John's Story</h3>
-                <p className="paragraph">
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-                  aliquam erat{" "}
-                </p>
-                <button className="readMore">Read more</button>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
