e0012e97727e6a5beeaacdfa254633f628d62740
Ali Taleb <71498603+ALImTALEB@users.noreply.github.com>
2025-03-29 14:51:01 +0100

    fix vercel depl

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/home-page/",
+  build: {
+    outDir: "dist", // ✅ Ensure it's correct
+  },
 });
